f287ca3a0bd5f7d3c44133ad0534934509d0083d
kevin.luy <kevin.luy@alibaba-inc.com>
2021-02-19 21:34:35 +0800

    编译阶段检查防止针对ast调整的可能 #7

--- src/main/java/io/github/luyiisme/script/sandbox/RejectASTTransformsCustomizer.java
+++ src/main/java/io/github/luyiisme/script/sandbox/RejectASTTransformsCustomizer.java
@@ -94,7 +94,7 @@ public class RejectASTTransformsCustomizer extends CompilationCustomizer {
                 for (String blockedAnnotation : getBlockedTransforms()) {
                     if (blockedAnnotation.equals(node.getType().getName()) || blockedAnnotation.endsWith(
                         "." + node.getType().getName())) {
-                        throw new UnsupportedOperationException("Insecure annotation '" + node.getType().getName() +
+                        throw new SecurityException("Insecure annotation '" + node.getType().getName() +
                             "' you can tweak the security sandbox to allow it. Read more about this in the " +
                             "documentation.");
                     }
@@ -113,7 +113,7 @@ public class RejectASTTransformsCustomizer extends CompilationCustomizer {
                 for (String blockedAnnotation : getBlockedTransforms()) {
                     if (blockedAnnotation.equals(an.getClassNode().getName()) || blockedAnnotation.endsWith(
                         "." + an.getClassNode().getName())) {
-                        throw new UnsupportedOperationException("Insecure annotation '" + an.getClassNode().getName() +
+                        throw new SecurityException("Insecure annotation '" + an.getClassNode().getName() +
                             "' you can tweak the security sandbox to allow it. Read more about this in the " +
                             "documentation.");
                     }
--- src/test/java/io/github/luyiisme/script/NoReflectionAllowedInterceptorTest.java
+++ src/test/java/io/github/luyiisme/script/NoReflectionAllowedInterceptorTest.java
@@ -1,13 +1,13 @@
 package io.github.luyiisme.script;
 
 import java.lang.reflect.Array;
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
 import io.github.luyiisme.script.groovy.GroovyScriptEngine;
 import io.github.luyiisme.script.management.ScriptManager;
 import io.github.luyiisme.script.sandbox.interceptor.NoReflectionAllowedInterceptor;
+import org.codehaus.groovy.control.MultipleCompilationErrorsException;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -36,8 +36,27 @@ public class NoReflectionAllowedInterceptorTest {
 
     @Test(expected = SecurityException.class)
     public void testReflectionCase() {
-        Array.newInstance(String.class,2);
-        ((ScriptManager)scriptEngine).registerScript("456", "java.lang.reflect.Array.newInstance(java.lang.String.class,2);");
+        Array.newInstance(String.class, 2);
+        ((ScriptManager)scriptEngine).registerScript("456",
+            "java.lang.reflect.Array.newInstance(java.lang.String.class,2);");
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("name", "jack");
+        Integer v = scriptEngine.invoke("456", params);
+    }
+
+    @Test(expected = MultipleCompilationErrorsException.class)
+    public void testRejectAST_Transform_Case() throws Throwable {
+        try {
+
+
+        ((ScriptManager)scriptEngine).registerScript("456",
+            "import groovy.transform.* \n @ASTTest(value = { \n def owner = node.declaringClass \n assert owner"
+                + ".fields.any { it.name == 'x' } \n }) \n @Field int x "
+                + " ");
+        }catch (Throwable e){
+            Assert.assertTrue(e.getMessage().contains("Insecure annotation"));
+            throw e;
+        }
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("name", "jack");
         Integer v = scriptEngine.invoke("456", params);
